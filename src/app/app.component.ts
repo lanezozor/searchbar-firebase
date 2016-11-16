b2705393749e15bc3d1aad70031d017666d141b8
Jorge Vergara <javebratt@gmail.com>
2016-11-16 14:51:05 -0500

    Initializes Firebase app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
 
+import firebase from 'firebase';
+
 
 @Component({
   template: `<ion-nav [root]="rootPage"></ion-nav>`
@@ -12,6 +14,18 @@ export class MyApp {
   rootPage = HomePage;
 
   constructor(platform: Platform) {
+
+    const config = {
+      apiKey: "AIzaSyBVglhmx9z5HYw0Tlme208NeUZow9rBZiE",
+      authDomain: "dummy-data-for-tests.firebaseapp.com",
+      databaseURL: "https://dummy-data-for-tests.firebaseio.com",
+      storageBucket: "dummy-data-for-tests.appspot.com",
+      messagingSenderId: "781486608293"
+    };
+    
+    firebase.initializeApp(config);
+
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
