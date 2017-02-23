dc0285567d8e4fc1b6bbf29ac0eea18e5b8b2146
Jorge Vergara <javebratt@gmail.com>
2017-02-23 17:20:55 -0500

    Update to V2 Final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
-import { StatusBar, Splashscreen } from 'ionic-native';
+import { StatusBar } from '@ionic-native/statusbar';
+import { Splashscreen } from '@ionic-native/splashscreen';
 
 import { HomePage } from '../pages/home/home';
 
